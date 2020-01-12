547089d7523d944221abc03cb6b7f248df1d284e
JollyRoger4u <petterssonroger@hotmail.com>
2020-01-12 01:10:48 +0100

    actual fix of untracked files

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'mageAdmin'
+    ],
+    'crypt' => [
+        'key' => '062344fe9305697bf3ff76d5dffa96b2'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1:3306',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '0df_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '0df_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        '127.0.0.1'
+    ],
+    'install' => [
+        'date' => 'Sat, 11 Jan 2020 16:56:49 +0000'
+    ]
+];
